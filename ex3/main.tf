2a90d5d2fdecc0f0b0276e0521c2fd1deb97d8d6
eneiss <emmaneiss2@gmail.com>
2023-10-18 23:17:13 -0400

    Ex3 - explicitly create floating IP

--- ex3/main.tf
+++ ex3/main.tf
@@ -1,12 +1,12 @@
+# ==================== INSTANCE
 resource "openstack_compute_instance_v2" "ex3" {
   name     = "ex3_instance"
   provider = openstack.ovh
 
-  image_name  = "Debian 10"
+  image_name  = var.image_name
   region      = var.region
   flavor_name = var.instance_type
   key_pair    = openstack_compute_keypair_v2.test_keypair.name
-  # security_groups = ["default"]   # not required?
 
   user_data = file("cloud-init.sh")
 
@@ -15,8 +15,23 @@ resource "openstack_compute_instance_v2" "ex3" {
   }
 }
 
+# ==================== KEY PAIR
 resource "openstack_compute_keypair_v2" "test_keypair" {
-  provider   = openstack.ovh
   name       = "ex3_keypair"
+  provider   = openstack.ovh
+  region     = var.region
   public_key = file("~/.ssh/id_rsa.pub")
+}
+
+# ==================== FLOATING IP
+resource "openstack_compute_floatingip_v2" "instance_ip" {
+  provider = openstack.ovh
+  region   = var.region
+  pool     = "public"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "association" {
+  provider    = openstack.ovh
+  floating_ip = openstack_compute_floatingip_v2.instance_ip.address
+  instance_id = openstack_compute_instance_v2.ex3.id
 }
--- ex3/variables.tf
+++ ex3/variables.tf
@@ -1,9 +1,14 @@
+variable "region" {
+  type    = string
+  default = "GRA7" # Gravelines, Nord de la France
+}
+
 variable "instance_type" {
   type    = string
   default = "d2-4" # Discovery type (d2-4: 4GB, 2vCPU)
 }
 
-variable "region" {
+variable "image_name" {
   type    = string
-  default = "GRA7" # Gravelines, Nord de la France
+  default = "Debian 10"
 }
